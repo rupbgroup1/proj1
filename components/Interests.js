cf8f405d96313d5adc909a3333662e654e07cd38
rupbgroup1 <60218339+rupbgroup1@users.noreply.github.com>
2020-05-03 14:00:47 +0300

    Fn
    fn

--- components/Interests.js
+++ components/Interests.js
@@ -8,56 +8,58 @@ import { View, StyleSheet, TouchableOpacity } from 'react-native';
 const Interest = (props) => {
     //one choice
     const [selectedSub, setSelectedSub] = useState(0);
-    //multii
+    //multi choice
     const [selectedMultiSub, setSelectedMultiSub] = useState();
+
     //set initial value to multi (from user data)
     useEffect(() => {
         //console.log("effect", props.initialInterest);
-        props.isMulti&& 
-        setSelectedMultiSub(props.initialInterest)
-    }, [] 
-     );
+        props.isMulti &&
+            setSelectedMultiSub(props.initialInterest)
+    }, []
+    );
 
-    //pass the updated interests selected (array) for multi only
-     useEffect(() => {
+    //pass the updated interests selected (array) for multi only when the array change
+    useEffect(() => {
         //console.log("selected=",selectedMultiSub);
-        props.isMulti&& props.callFetch(selectedMultiSub)
-    }, [selectedMultiSub] 
-    
-     );
+        props.isMulti && props.callFetch(selectedMultiSub)
+    }, [selectedMultiSub]
 
-     subInterestPressedTwice=(interestId)=>{
-        
-        const check = selectedMultiSub.filter(e => e.Id===interestId);
-        //console.log("check=",check);
+    );
+
+    //checks if the interest is selected on the second time
+    subInterestPressedTwice = (interestId) => {
+        const check = selectedMultiSub.filter(e => e.Id === interestId);
         return check.length;
-     }
+    }
+
     //when i is pressed it checks wether it's multi/single and updates the value
     handleSubPress = (interestId) => {
-        const iObj = {Id:interestId};
+        const iObj = { Id: interestId };
         if (props.isMulti) {
             //it check if the user press on the same i to cancel it or selected new i
-            if  (subInterestPressedTwice(interestId)>0) {
+            if (subInterestPressedTwice(interestId) > 0) {
                 //remove from array
                 setSelectedMultiSub(selectedMultiSub.filter(item => item.Id !== interestId));
             }
             else {
                 //add to array
                 setSelectedMultiSub(selectedMultiSub => [...selectedMultiSub, iObj]);
-                
+
             }
         }
+        //not multi
         else {
             //it check if the user press on the same i to cancel it or selected new i
             //update state
-            selectedSub===interestId? setSelectedSub(0) : setSelectedSub(interestId);
+            selectedSub === interestId ? setSelectedSub(0) : setSelectedSub(interestId);
             //update parent
             props.cleanUserName();
-            selectedSub===interestId? props.callFetch(0) :props.callFetch(interestId);
+            selectedSub === interestId ? props.callFetch(0) : props.callFetch(interestId);
         }
 
     };
-    
+
 
 
     return (
@@ -79,16 +81,17 @@ const Interest = (props) => {
                 {props.subInArray !== null && props.subInArray.map((Interest, i) =>
                     //subCat
                     <OurButton
-                    //change color when selected
-                        style={(!props.isMulti && selectedSub === Interest.Id) || (props.isMulti && subInterestPressedTwice(Interest.Id)>0)
-                             ? styles.subButton : styles.intrestButtons}
+                        //change color when selected
+                        style={(!props.isMulti && selectedSub === Interest.Id) || (props.isMulti && subInterestPressedTwice(Interest.Id) > 0)
+                            ? styles.subButton : styles.intrestButtons}
                         title={Interest.Subintrest}
                         key={Interest.Id}
-                        //get from parent= is it multi selected?
+                        //get from parent= is it multi selected? 
                         isMulti={props.isMulti}
                         //change value when pressed
-                        onPress={() => handleSubPress(Interest.Id)}>
-                    
+                        onPress={() => handleSubPress(Interest.Id)}
+                    >
+
                         {Interest.Subintrest}
                     </OurButton>
 
@@ -114,8 +117,8 @@ const styles = StyleSheet.create({
         borderLeftWidth: 1,
         shadowColor: "#000",
         shadowOffset: {
-	    width: 0,
-	    height: 0.5,
+            width: 0,
+            height: 0.5,
         },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
